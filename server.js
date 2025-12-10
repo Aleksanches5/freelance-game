d0b443136e2ee0c354793fd2d98a2dca21969d5f
Aleksanches5 <yakosasha05@gmail.com>
2025-12-10 21:21:36 +0300

    Fix server routing for Amvera

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Amvera обычно пробрасывает PORT, но на всякий случай ставим 3000 по умолчанию
 const PORT = process.env.PORT || 3000;
 
-// Раздаём статические файлы из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Для любых маршрутов отдаём index.html (SPA)
-app.get('/*', (req, res) => {
+// Отдаём index.html только на корне
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
